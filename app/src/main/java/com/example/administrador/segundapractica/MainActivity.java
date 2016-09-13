d8af5e9c7d3e3faca351012d6c89daf47e1cb859
SERATIC-PC8\Administrador <helberth.henao@seratic.com>
2016-09-13 18:26:06 -0500

    Adapters

--- app/src/main/java/com/example/administrador/segundapractica/MainActivity.java
+++ app/src/main/java/com/example/administrador/segundapractica/MainActivity.java
@@ -37,46 +37,46 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         data.add(peliculas);
 
         Peliculas peliculas1 = new Peliculas();
-        peliculas.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Festaticos.elperiodico.com%2Fresources%2Fjpg%2F0%2F9%2Ftelevision-pelicula-hotel-transylvania-1451665834890.jpg&imgrefurl=http%3A%2F%2Fwww.elperiodico.com%2Fes%2Fnoticias%2Fque-echan-en-la-tele%2Fpeliculas-hoy-tv-sabado-2-enero-2016-4788720&docid=qdaefTunUENdTM&tbnid=L42j8jAoZoDb1M%3A&w=646&h=361&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwgCKAAwAA&iact=mrc&uact=8");
-        peliculas.setNombre("Pelicula 1");
-        peliculas.setCategoria("Categoria 1");
-        peliculas.setDescripcion("Descripcion 1");
-        peliculas.setFecha("Fecha 1");
+        peliculas1.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Festaticos.elperiodico.com%2Fresources%2Fjpg%2F0%2F9%2Ftelevision-pelicula-hotel-transylvania-1451665834890.jpg&imgrefurl=http%3A%2F%2Fwww.elperiodico.com%2Fes%2Fnoticias%2Fque-echan-en-la-tele%2Fpeliculas-hoy-tv-sabado-2-enero-2016-4788720&docid=qdaefTunUENdTM&tbnid=L42j8jAoZoDb1M%3A&w=646&h=361&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwgCKAAwAA&iact=mrc&uact=8");
+        peliculas1.setNombre("Pelicula 1");
+        peliculas1.setCategoria("Categoria 1");
+        peliculas1.setDescripcion("Descripcion 1");
+        peliculas1.setFecha("Fecha 1");
         data.add(peliculas1);
 
 
         Peliculas peliculas2 = new Peliculas();
-        peliculas.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Fk30.kn3.net%2Ftaringa%2F3%2F6%2F0%2F4%2F1%2FF%2Fdisco00%2F080.jpg&imgrefurl=http%3A%2F%2Fwww.taringa.net%2Fposts%2Ftv-peliculas-series%2F18858286%2F20-peliculas-mas-esperadas-para-2016.html&docid=PuZh38y3fnzs8M&tbnid=B48d-E-GrJ74bM%3A&w=1080&h=720&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwg3KDQwNA&iact=mrc&uact=8");
-        peliculas.setNombre("Pelicula 1");
-        peliculas.setCategoria("Categoria 1");
-        peliculas.setDescripcion("Descripcion 1");
-        peliculas.setFecha("Fecha 1");
+        peliculas2.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Fk30.kn3.net%2Ftaringa%2F3%2F6%2F0%2F4%2F1%2FF%2Fdisco00%2F080.jpg&imgrefurl=http%3A%2F%2Fwww.taringa.net%2Fposts%2Ftv-peliculas-series%2F18858286%2F20-peliculas-mas-esperadas-para-2016.html&docid=PuZh38y3fnzs8M&tbnid=B48d-E-GrJ74bM%3A&w=1080&h=720&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwg3KDQwNA&iact=mrc&uact=8");
+        peliculas2.setNombre("Pelicula 1");
+        peliculas2.setCategoria("Categoria 1");
+        peliculas2.setDescripcion("Descripcion 1");
+        peliculas2.setFecha("Fecha 1");
         data.add(peliculas2);
 
 
         Peliculas peliculas3 = new Peliculas();
-        peliculas.setImgUrl("");
-        peliculas.setNombre("Pelicula 1");
-        peliculas.setCategoria("Categoria 1");
-        peliculas.setDescripcion("Descripcion 1");
-        peliculas.setFecha("Fecha 1");
+        peliculas3.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Fk30.kn3.net%2Ftaringa%2F3%2F6%2F0%2F4%2F1%2FF%2Fdisco00%2F080.jpg&imgrefurl=http%3A%2F%2Fwww.taringa.net%2Fposts%2Ftv-peliculas-series%2F18858286%2F20-peliculas-mas-esperadas-para-2016.html&docid=PuZh38y3fnzs8M&tbnid=B48d-E-GrJ74bM%3A&w=1080&h=720&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwg3KDQwNA&iact=mrc&uact=8");
+        peliculas3.setNombre("Pelicula 1");
+        peliculas3.setCategoria("Categoria 1");
+        peliculas3.setDescripcion("Descripcion 1");
+        peliculas3.setFecha("Fecha 1");
         data.add(peliculas3);
 
         Peliculas peliculas4 = new Peliculas();
-        peliculas.setImgUrl("");
-        peliculas.setNombre("Pelicula 1");
-        peliculas.setCategoria("Categoria 1");
-        peliculas.setDescripcion("Descripcion 1");
-        peliculas.setFecha("Fecha 1");
+        peliculas4.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Fk30.kn3.net%2Ftaringa%2F3%2F6%2F0%2F4%2F1%2FF%2Fdisco00%2F080.jpg&imgrefurl=http%3A%2F%2Fwww.taringa.net%2Fposts%2Ftv-peliculas-series%2F18858286%2F20-peliculas-mas-esperadas-para-2016.html&docid=PuZh38y3fnzs8M&tbnid=B48d-E-GrJ74bM%3A&w=1080&h=720&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwg3KDQwNA&iact=mrc&uact=8");
+        peliculas4.setNombre("Pelicula 1");
+        peliculas4.setCategoria("Categoria 1");
+        peliculas4.setDescripcion("Descripcion 1");
+        peliculas4.setFecha("Fecha 1");
         data.add(peliculas4);
 
 
         Peliculas peliculas5 = new Peliculas();
-        peliculas.setImgUrl("");
-        peliculas.setNombre("Pelicula 1");
-        peliculas.setCategoria("Categoria 1");
-        peliculas.setDescripcion("Descripcion 1");
-        peliculas.setFecha("Fecha 1");
+        peliculas5.setImgUrl("https://www.google.com.co/imgres?imgurl=http%3A%2F%2Fk30.kn3.net%2Ftaringa%2F3%2F6%2F0%2F4%2F1%2FF%2Fdisco00%2F080.jpg&imgrefurl=http%3A%2F%2Fwww.taringa.net%2Fposts%2Ftv-peliculas-series%2F18858286%2F20-peliculas-mas-esperadas-para-2016.html&docid=PuZh38y3fnzs8M&tbnid=B48d-E-GrJ74bM%3A&w=1080&h=720&bih=606&biw=1242&ved=0ahUKEwj1jsWapY3PAhWE8z4KHaTLATY4yAEQMwg3KDQwNA&iact=mrc&uact=8");
+        peliculas5.setNombre("Pelicula 1");
+        peliculas5.setCategoria("Categoria 1");
+        peliculas5.setDescripcion("Descripcion 1");
+        peliculas5.setFecha("Fecha 1");
         data.add(peliculas5);
 
         adapter = new ListPeliculasAdapter(data, this);
